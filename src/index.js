2dcfb4ada88a3b3f888b2126153b18465feb1c56
Ramzan <ramzan4ik95@mail.ru>
2022-06-07 16:35:39 +0300

    исправил ошибку с стилями

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style/index.scss";
-import "./style/media.scss";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store/store";
